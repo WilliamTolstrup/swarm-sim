3c29566d495d90491801aa6e31f03d198472fcc8
William Tolstrup <williamtolstrup@hotmail.com>
2022-04-03 20:19:00 +0200

    added separated planner for multi robot simulation

---
+++ remote/launch/multi_launch.py
@@ -0,0 +1,179 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Example for spawing multiple robots in Gazebo.
+
+This is an example on how to create a launch file for spawning multiple robots into Gazebo
+and launch multiple instances of the navigation stack, each controlling one robot.
+The robots co-exist on a shared environment and are controlled by independent nav stacks
+"""
+
+import os
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess, GroupAction,
+                            IncludeLaunchDescription, LogInfo)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+
+def generate_robot_list(num_robots):
+
+    robot_list = []
+
+    for i in range(num_robots):
+        robot_name = "robot" + str(i)
+        x_pos = float(i)
+        robot_list.append({'name': robot_name, 'x_pose': x_pos, 'y_pose': 0.0, 'z_pose': 0.01})
+
+    return robot_list
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('remote')
+    launch_dir = os.path.join(bringup_dir, 'launch')
+
+    # Names and poses of the robots
+
+    robots = generate_robot_list(5)
+
+    # Simulation settings
+    world = LaunchConfiguration('world')
+    simulator = LaunchConfiguration('simulator')
+
+    # On this example all robots are launched with the same settings
+    map_yaml_file = LaunchConfiguration('map')
+    autostart = LaunchConfiguration('autostart')
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+    log_settings = LaunchConfiguration('log_settings', default='true')
+
+    # Declare the launch arguments
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(bringup_dir, 'worlds', 'world_only.model'),
+        description='Full path to world file to load')
+
+    declare_simulator_cmd = DeclareLaunchArgument(
+        'simulator',
+        default_value='gazebo',
+        description='The simulator to use (gazebo or gzserver)')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
+        description='Full path to map file to load')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='false',
+        description='Automatically startup the stacks')
+
+    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+        'use_robot_state_pub',
+        default_value='True',
+        description='Whether to start the robot state publisher')
+
+    # Declare a list of arguments for each robot
+    declare_params_list = []
+    for robot in robots:
+        declare_params_list.append(
+            DeclareLaunchArgument(
+                f"{robot['name']}_params_file",
+                default_value=os.path.join(bringup_dir, 'params', f"nav2_multirobot_params_{robot['name']}.yaml"),
+                description=f"Full path to the ROS2 parameters file to use for robot{robot['name']} launched nodes"))
+
+    # Start Gazebo with plugin providing the robot spawing service
+    start_gazebo_cmd = ExecuteProcess(
+        cmd=[simulator, '--verbose', '-s', 'libgazebo_ros_init.so',
+                                     '-s', 'libgazebo_ros_factory.so', world],
+        output='screen')
+
+    # Define commands for spawing the robots into Gazebo
+    spawn_robots_cmds = []
+    for robot in robots:
+        spawn_robots_cmds.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(os.path.join(launch_dir, 'spawn_tb3_launch.py')),
+                launch_arguments={
+                                  'x_pose': TextSubstitution(text=str(robot['x_pose'])),
+                                  'y_pose': TextSubstitution(text=str(robot['y_pose'])),
+                                  'z_pose': TextSubstitution(text=str(robot['z_pose'])),
+                                  'robot_name': robot['name'],
+                                  'turtlebot_type': TextSubstitution(text='waffle')
+                                  }.items()))
+
+    # Define commands for launching the navigation instances
+    nav_instances_cmds = []
+    for robot in robots:
+        params_file = LaunchConfiguration(f"{robot['name']}_params_file")
+
+        group = GroupAction([
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(os.path.join(launch_dir, 'simulation_launch.py')),
+                launch_arguments={'namespace': robot['name'],
+                                  'use_namespace': 'True',
+                                  'map': map_yaml_file,
+                                  'use_sim_time': 'True',
+                                  'params_file': params_file,
+                                  'autostart': autostart,
+                                  'use_simulator': 'False',
+                                  'headless': 'False',
+                                  'use_robot_state_pub': use_robot_state_pub}.items()),
+
+            LogInfo(
+                condition=IfCondition(log_settings),
+                msg=['Launching ', robot['name']]),
+            LogInfo(
+                condition=IfCondition(log_settings),
+                msg=[robot['name'], ' map yaml: ', map_yaml_file]),
+            LogInfo(
+                condition=IfCondition(log_settings),
+                msg=[robot['name'], ' params yaml: ', params_file]),
+            LogInfo(
+                condition=IfCondition(log_settings),
+                msg=[robot['name'], ' using robot state pub: ', use_robot_state_pub]),
+            LogInfo(
+                condition=IfCondition(log_settings),
+                msg=[robot['name'], ' autostart: ', autostart])
+        ])
+
+        nav_instances_cmds.append(group)
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_simulator_cmd)
+    ld.add_action(declare_world_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_robot_state_pub_cmd)
+
+    # Declare launch options for robot parameters
+    for declare_param_list in declare_params_list:
+        ld.add_action(declare_param_list)
+
+    # Add the actions to start gazebo, robots and simulations
+    ld.add_action(start_gazebo_cmd)
+
+    for spawn_robot_cmd in spawn_robots_cmds:
+        ld.add_action(spawn_robot_cmd)
+
+    for simulation_instance_cmd in nav_instances_cmds:
+        ld.add_action(simulation_instance_cmd)
+
+    return ld
